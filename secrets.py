e062adb1c6d6e5325b8ba3b0a44754ea44acd54e
Jasper Juchem <Jasper_Juchem@hotmail.com>
2020-10-29 16:24:43 +0100

    template secrets

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# User specific parameters
+email_from = 'example@example.com'
+email_to = ['example1@example.com', 'example2@example.com']
+password = 'my_password'
+
+# Email parameters
+host = 'my_host_name'
+port = 123
