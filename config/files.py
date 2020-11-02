8def727fa7f4e71fcd4c1f93434627621f4cd420
Jasper Juchem <Jasper_Juchem@hotmail.com>
2020-11-02 22:24:41 +0100

    generalize template src files

---
+++ config/files.py
@@ -0,0 +1,2 @@
+# Path to the folder where the measurement files should be saved
+path = "C:\\the\\path\\to\\the\\folder\\where\\to\\save\\"
--- config/mail.py
+++ config/mail.py
@@ -1,4 +1,4 @@
 # User specific parameters
-email_from = "measurements.dysc@UGent.be"
-email_to = ["jasper.Juchem@UGent.be"]
-email_cc = ["measurements.dysc@UGent.be"]
+email_from = "example1@example1.com"
+email_to = ["example2@example2.be", "example3@example3.com"]
+email_cc = ["example4@example4.com"]
