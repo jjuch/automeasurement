b21a7494ee1275ed715a2a22ef71c3dfd878fe26
Jasper Juchem <Jasper_Juchem@hotmail.com>
2020-11-03 00:03:34 +0100

    add env activation is bash

---
+++ check_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Path to virtual environment activation script
+activate_path='ENV/Scripts/activate'
+#TODO: Check if path exists, if not create
+
+# Function calling the activation script
+activate () {
+    source $activate_path
+}
+
+# Check if the virtualenv is activated
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "No VIRTUAL_ENV set"
+    activate
+else
+    echo "VIRTUAL_ENV is set"
+fi
+
+# Run python script as module
+python -m src.daq.daq_task
