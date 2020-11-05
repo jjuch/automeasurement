1d3478b5e5add4ffc02d3c5093515beba82347e0
jjuchem <Jasper.Juchem@UGent.be>
2020-11-05 15:42:03 +0100

    bug fix mail client

--- src/mail/mail_client.py
+++ src/mail/mail_client.py
@@ -113,5 +113,5 @@ if __name__ == "__main__":
     import config.secrets as cfg
 
     mc = MailClient(cfg.host, cfg.port, cfg.user, cfg.password)
-    mc.send_test(cfg.email_from, cfg.email_to, cc_email=cfg.email_cc)
+    mc.send_test_email(cfg.email_from, cfg.email_to, cc_email=cfg.email_cc)
     mc.quit_client()
